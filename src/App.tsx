bf3776e2341a0d6ed8d5660258bbf7944064ac4d
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-10 16:17:25 +0300

    003-API be able to request with Search component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import { ChangeEvent, SyntheticEvent } from "react";
+import { searchCompanies } from "./api";
+import { CompanySearch } from "./company";
 import CardList from "./Components/CardList/CardList";
 import { Search } from "./Components/Search/Search";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[] | string>(
+    []
+  );
+  const [serverError, setServerError] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
+  };
+
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
     </div>
   );
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,20 +1,16 @@
 import { ChangeEvent, SyntheticEvent } from "react";
-import { useState } from "react";
 
-interface Props {}
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-export const Search: React.FC<Props> = (props: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
-
+export const Search: React.FC<Props> = ({
+  onClick,
+  search,
+  handleChange,
+}: Props): JSX.Element => {
   return (
     <div>
       <input value={search} onChange={(e) => handleChange(e)}></input>
