2d80d6aff167ed34cd7462b18fc430d95610cae3
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-10-11 01:16:53 +0300

    014-IncomeStatement added.

--- src/Components/IncomeStatement/IncomeStatement.tsx
+++ src/Components/IncomeStatement/IncomeStatement.tsx
@@ -1,7 +1,56 @@
+import { CompanyIncomeStatement } from "../../company";
+import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getIncomeStatement } from "../../api";
+import Table from "../Table/Table";
+
 type Props = {};
 
+const configs = [
+  {
+    label: "Date",
+    render: (company: CompanyIncomeStatement) => company.date,
+  },
+  {
+    label: "Total Revenue",
+    render: (company: CompanyIncomeStatement) => company.revenue,
+  },
+  {
+    label: "Net Income",
+    render: (company: CompanyIncomeStatement) => company.netIncome,
+  },
+  {
+    label: "Operating Expenses",
+    render: (company: CompanyIncomeStatement) => company.operatingExpenses,
+  },
+  {
+    label: "Cost of Revenue",
+    render: (company: CompanyIncomeStatement) => company.netIncome,
+  },
+];
+
 function IncomeStatement({}: Props) {
-  return <div>IncomeStatement</div>;
+  const ticker = useOutletContext<string>();
+  const [incomeStatement, setIncomeStatement] =
+    useState<CompanyIncomeStatement[]>();
+
+  useEffect(() => {
+    const getRatios = async () => {
+      const result = await getIncomeStatement(ticker!);
+      setIncomeStatement(result!.data);
+    };
+    getRatios();
+  }, []);
+
+  return (
+    <>
+      {incomeStatement ? (
+        <Table config={configs} data={incomeStatement} />
+      ) : (
+        <h1>Could not find income statement :(</h1>
+      )}
+    </>
+  );
 }
 
 export default IncomeStatement;
--- src/api.tsx
+++ src/api.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {
+  CompanyIncomeStatement,
   CompanyKeyMetrics,
   CompanyKeyRatios,
   CompanyProfile,
@@ -46,6 +47,19 @@ export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
       `https://financialmodelingprep.com/api/v3/key-metrics-ttm/AAPL?limit=40&apikey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=40&apikey=${
         import.meta.env.VITE_API_KEY
       }`
     );
